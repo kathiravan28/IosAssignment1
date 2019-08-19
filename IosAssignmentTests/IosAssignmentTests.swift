7820565b14014873f40139c2d450cb8bb7736cad
kathir <kathir007crystals@gmail.com>
2019-08-18 22:09:17 -0700

    Testcase

--- IosAssignmentTests/IosAssignmentTests.swift
+++ IosAssignmentTests/IosAssignmentTests.swift
@@ -10,12 +10,18 @@ import XCTest
 @testable import IosAssignment
 
 class IosAssignmentTests: XCTestCase {
+    
+     var sut: URLSession!
 
     override func setUp() {
+         super.setUp()
+         sut = URLSession(configuration: .default)
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+          sut = nil
+         super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
@@ -30,5 +36,32 @@ class IosAssignmentTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    func testValidCallToiTunesGetsHTTPStatusCode200() {
+        // given
+        let url =
+            URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        // 3
+        wait(for: [promise], timeout: 5)
+    }
+    
 }
+
