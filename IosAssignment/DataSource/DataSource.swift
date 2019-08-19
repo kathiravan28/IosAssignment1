5062320629af19eceba7e0bcbbfb81b7a57bffb0
kathir <kathir007crystals@gmail.com>
2019-08-18 21:49:07 -0700

    Added TableView DataSource

--- IosAssignment/DataSource/DataSource.swift
+++ IosAssignment/DataSource/DataSource.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+class TableViewDataSource<Cell :UITableViewCell,RowsModel> : NSObject, UITableViewDataSource, UITableViewDelegate {
+    
+    private var cellIdentifier :String!
+    private var items :[RowsModel]!
+    var configureCell :(Cell,RowsModel) -> ()
+    
+    init(cellIdentifier :String, items :[RowsModel], configureCell: @escaping (Cell,RowsModel) -> ()) {
+        
+        self.cellIdentifier = cellIdentifier
+        self.items = items
+        self.configureCell = configureCell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as! Cell
+        let item = self.items[indexPath.row]
+        self.configureCell(cell,item)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 118
+    }
+}
